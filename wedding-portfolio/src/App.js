4a513494f3fb5ca505af126eedd216fbe261b0e9
Laura Daugherty <lauradaugherty6@gmail.com>
2019-07-29 11:13:11 -0700

    Setting up App file structure

--- wedding-portfolio/src/App.js
+++ wedding-portfolio/src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { longStackSupport } from 'q';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
+
+// STATE (contexts to pass?):
+// 		weddings
+// 		user
+
+// NAVBAR
+    // simple log in or Log out
+    // if Logged in has links to 
+    //   add
+    //   log out 
+    //   guest view
+    //   portfolio view
+    // IF Logged OUT
+          //logIn button
+
+          
+// 	No Auth Routes:
+// 		“/Login” {Login}
+// 		“/Register” {Register}
+// 		“/Guest” {Guest}
+// 	With Auth Private Routes:
+// 		“/Portfolio”{Portfolio}
+// 		“/weddingForm”{weddingForm}
+		
+// 	Logout:
+// 		logout=(event) => {
+// 		this.props.history.push(‘/guest’)
+// 		localStorage.clear()
+// 		}
+// 	checkLogin:
+// 		if user truths state => render Component otherwise send to “/login”
+
+//  user: {
+//   userId: “”,
+//    firstName: "",
+//    lastName: "",
+//    username: "",
+//    password: "",
+//    weddings: [
+//      {
+//        weddingId: "",
+//        plannerId: “”,
+//        date: “”,
+//        image: "",
+//        description: "",
+//        vendors: "",
+//        theme: "",
+//        location: ""
+//      }
+//    ]
+//  }
