2f8815a6142c8850f7acce92fd1494fce376d97c
Desiree Morris <desiquinncompany@gmail.com>
2019-07-30 16:29:52 -0400

    fixed a few errors

--- wedding-portfolio/src/index.js
+++ wedding-portfolio/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 import { BrowserRouter, withRouter } from "react-router-dom"
