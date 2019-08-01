c946998181b48f428fb1ac2f47420df267062e58
Laura Daugherty <lauradaugherty6@gmail.com>
2019-08-01 13:52:53 -0700

    WIP Working on Navbar design

--- wedding-portfolio/src/Components/Global/NavBar.js
+++ wedding-portfolio/src/Components/Global/NavBar.js
@@ -20,7 +20,7 @@ export const NavBar = props => {
             header as={NavLink} 
             exact to="/portfolioView" 
             activeClassName="active"
-            >
+          >
             Portfolio
           </Menu.Item>
 
@@ -28,47 +28,55 @@ export const NavBar = props => {
             header as={NavLink} 
             exact to="/weddingForm" 
             activeClassName="active"
-            >
-            Add Wedding
+          >
+            Add Weddings
           </Menu.Item>
 
-          <Button size='tiny' onClick={logout}>Logout</Button>
+          <Menu.Menu position="right">
+            <Menu.Item
+              name="Logout"
+              onClick={logout}
+              />
+          </Menu.Menu>
         </>
       )
     } else {
       return (
-        <Menu.Item 
-        header as={NavLink} 
-        exact to="/" 
-        activeClassName="active"
-        >
-        GuestView
-      </Menu.Item>
+        <>
+          <Menu.Item 
+            header as={NavLink} 
+            exact to="/" 
+            activeClassName="active"
+          >
+            GuestView
+          </Menu.Item>
+          <Menu.Item 
+            header as={NavLink} 
+            exact to="/login" 
+            activeClassName="active"
+            
+          >
+            Login
+          </Menu.Item>
+          <Menu.Item 
+            header as={NavLink} 
+            exact to="/register" 
+            activeClassName="active"
+            >
+            Sign Up
+          </Menu.Item>
+        </>
       )
     }
   }
 
+  // const {color} = "#DCB5F9"
+
   return (
     <div>
     <Segment attached>
-      <Menu>
-        <Menu.Item 
-          header as={NavLink} 
-          exact to="/login" 
-          activeClassName="active"
-          >
-          Login
-        </Menu.Item>
-        <Menu.Item 
-          header as={NavLink} 
-          exact to="/register" 
-          activeClassName="active"
-          >
-          Sign Up
-        </Menu.Item>
+      <Menu pointing >
         {portfolioAccess()}
-
-
       </Menu>
     </Segment>
   </div>
